cf0612ada73b36adb28128412de0b8374f7d84af
LunaAstro <hhcbnu@gmail.com>
2017-01-20 12:06:07 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='rochelobe',
+      version='0.1',
+      description='Roche potential of a binary system',
+      author='Huanchen Hu',
+      author_email='hhcbnu@gmail.com',
+      url='https://github.com/LunaAstro/rochelobe.git',
+      scripts=['rochelobe']
+      )
